726fee68d987ae4c3aa14c433d143eba70a0462b
Abhishek Kumar <abhikrnigam@gmail.com>
2020-05-27 06:56:33 +0530

    First commit of the project.
    MainScreen and other two forms are created.

---
+++ lib/MainScreen.dart
@@ -0,0 +1,80 @@
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black12,
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 100),
+              child: Material(
+                color: Colors.black12,
+                child: Text(
+                  "Lost & Found",
+                  style: GoogleFonts.monoton(
+                    fontSize: 45,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                  )
+                ),
+              ),
+            ),
+            Spacer(),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+              ),
+              child: RawMaterialButton(
+                onPressed: (){
+//                  Navigator.push(context, MaterialPageRoute(builder: (context)=>MainScreen()));
+                },
+                elevation: 6,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal:25.0,vertical: 10),
+                  child: Text("Lost Something",
+                    style: GoogleFonts.robotoSlab(
+                        fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),),
+                ),
+              ),
+            ),
+                Container
+                  (
+                  margin: EdgeInsets.only(top:30),
+                  decoration: BoxDecoration
+                    (
+                  borderRadius: BorderRadius.circular(20),
+                  color:Colors.white,
+                    ),
+                      child: RawMaterialButton
+                        (
+                        onPressed: (){;},
+                        elevation: 6,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal:25.0,vertical: 10),
+                          child: Text(
+                              "Found Something",
+                            style: GoogleFonts.robotoSlab(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        ),
+                ),
+
+            Spacer(flex: 3,),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'MainScreen.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+              accentColorBrightness: Brightness.dark,
+      ),
+      home: MainScreen(),
+    );
+
+  }
+}
+
