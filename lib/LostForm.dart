8d3f885215615e023baf2b313420c19732a66e68
Abhishek Kumar <abhikrnigam@gmail.com>
2020-05-28 20:27:45 +0530

    Added the firebase services to the application Made the Column a scrollable widget and addded the conatiners for image addition. Till this point application is working fine.

--- lib/LostForm.dart
+++ lib/LostForm.dart
@@ -1,7 +1,13 @@
+
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
+
 class LostForm extends StatefulWidget {
 
 
@@ -10,6 +16,10 @@ class LostForm extends StatefulWidget {
 }
 
 class _LostFormState extends State<LostForm> {
+
+  bool image1uploaded=false;
+  bool image2uploaded=false;
+  Timestamp timeFromDate;
   String dataformatted;
   @override
   void initState() {
@@ -21,14 +31,15 @@ class _LostFormState extends State<LostForm> {
   {
     DateTime dateLost=DateTime.now();
      dataformatted=DateFormat('dd-MM-yyyy').format(dateLost);
+     timeFromDate=Timestamp.fromDate(dateLost);
   }
-
+  List listOfImages=[];
   String name;
-
   String item;
   String location;
   String description;
-
+  File image1;
+  File image2;
   TextEditingController controller1=new TextEditingController();
 
   TextEditingController controller2=new TextEditingController();
@@ -48,10 +59,55 @@ class _LostFormState extends State<LostForm> {
 
     setState(() {
     dataformatted=DateFormat('dd-MM-yyyy').format(picked);
+    timeFromDate=Timestamp.fromDate(picked);
     });
   }
 
+  Future<Widget> getImage1()async{
+    if(!image1uploaded) {
+      var img = await ImagePicker.pickImage(source: ImageSource.gallery);
+      image1 = img;
+      image1uploaded=true;
+      return Image.file(image1);
+    }
+    else{
+      return IconButton(
+        icon: Icon(
+          Icons.add_circle,
+          size: 35,
+          color: Colors.white,
+        ),
+        onPressed: (){
+          setState(() {
+            getImage1();
+          });
+        },
+      );
+    }
+  }
 
+  Future<Widget> getImage2()async{
+    if(!image2uploaded) {
+      var img = await ImagePicker.pickImage(source: ImageSource.gallery);
+      image2 = img;
+      image2uploaded=true;
+      return Image.file(image2);
+    }
+    else{
+      return IconButton(
+        icon: Icon(
+          Icons.add_circle,
+          size: 35,
+          color: Colors.white,
+        ),
+        onPressed: (){
+          setState(() {
+            getImage2();
+          });
+        },
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,208 +123,297 @@ class _LostFormState extends State<LostForm> {
       ),
       body: SafeArea(
           child:
-          Column(
+          ListView(
+            children: <Widget>[ Column(
         children: <Widget>[
-          Container(
-            margin: EdgeInsets.only(top: 20,bottom: 5,left: 0,right: 250),
-            child: Text(
-              "Enter your name",
-                style: GoogleFonts.poppins(
-                  fontSize: 15,
-                  color: Colors.white,
-                ),
-                ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 15,vertical: 2),
-            child: TextField(
-              cursorColor: Colors.white,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.white,
-              ),
-              onChanged: (NAME){
-                name=NAME;
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                ),
-                filled: true,
-                fillColor: Colors.black54,
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                ),
-              ),
-              controller: controller1,
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(top: 20,bottom: 5,left: 0,right: 200),
-            child: Text(
-            "What/Whom you lost ?",
-              style: GoogleFonts.poppins(
-                fontSize: 15,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 15,vertical: 2),
-            child: TextField(
-              cursorColor: Colors.white,
-              style: TextStyle(
-                color: Colors.white,
-              ),
-              onChanged: (ITEM){
-                item=ITEM;
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                ),
-                filled: true,
-                fillColor: Colors.black54,
-                hintStyle: TextStyle(
-                  color: Colors.grey,
-                ),
-              ),
-              controller: controller2,
-            ),
-          ),
-          Align(
-              alignment: Alignment.centerLeft,
-            child: Container(
-              margin: EdgeInsets.only(top: 20,left: 30,),
+            Container(
+              margin: EdgeInsets.only(top: 20,bottom: 5,left: 0,right: 250),
               child: Text(
-                  "When?",
-                style: GoogleFonts.poppins(
+                "Enter your name",
+                  style: GoogleFonts.poppins(
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
+                  ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 15,vertical: 2),
+              child: TextField(
+                cursorColor: Colors.white,
+                style: TextStyle(
+                  fontSize: 16,
                   color: Colors.white,
+                ),
+                onChanged: (NAME){
+                  name=NAME;
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                  filled: true,
+                  fillColor: Colors.black54,
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                controller: controller1,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20,bottom: 5,left: 0,right: 200),
+              child: Text(
+              "What/Whom you lost ?",
+                style: GoogleFonts.poppins(
                   fontSize: 15,
+                  color: Colors.white,
                 ),
               ),
             ),
-          ),
-          Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(left:20),
-                child: Card(
-                  color: Colors.black54,
-                  elevation: 6,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical:10,horizontal: 15),
-                    child: Text(
-                      "$dataformatted",
-                      style: GoogleFonts.poppins(
-                        color: Colors.white,
-                        fontSize: 25,
-                      ),
-                    ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 15,vertical: 2),
+              child: TextField(
+                cursorColor: Colors.white,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+                onChanged: (ITEM){
+                  item=ITEM;
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                  filled: true,
+                  fillColor: Colors.black54,
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                controller: controller2,
+              ),
+            ),
+            Align(
+                alignment: Alignment.centerLeft,
+              child: Container(
+                margin: EdgeInsets.only(top: 20,left: 30,),
+                child: Text(
+                    "When?",
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 15,
                   ),
                 ),
               ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Container(
-                  margin: EdgeInsets.only(bottom: 20,left: 18,top: 20),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(18),
-                  ),
-                  child: RawMaterialButton(
-                    elevation: 20,
-                    onPressed: (){
-                      _selectDate(context);
-                    },
+            ),
+            Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(left:20),
+                  child: Card(
+                    color: Colors.black54,
+                    elevation: 6,
                     child: Padding(
-                      padding: const EdgeInsets.all(15.0),
+                      padding: const EdgeInsets.symmetric(vertical:10,horizontal: 15),
                       child: Text(
-                        "Select Date",
+                        "$dataformatted",
                         style: GoogleFonts.poppins(
-                          fontSize: 20,
                           color: Colors.white,
+                          fontSize: 25,
                         ),
                       ),
                     ),
                   ),
                 ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: 20,left: 18,top: 20),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: RawMaterialButton(
+                      elevation: 20,
+                      onPressed: (){
+                        _selectDate(context);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Text(
+                          "Select Date",
+                          style: GoogleFonts.poppins(
+                            fontSize: 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                margin: EdgeInsets.only(bottom: 5,left: 21),
+                child: Text(
+                  "Where ?",
+                  style: GoogleFonts.poppins(
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
+                ),
               ),
-            ],
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Container(
-              margin: EdgeInsets.only(bottom: 5,left: 21),
-              child: Text(
-                "Where ?",
-                style: GoogleFonts.poppins(
-                  fontSize: 15,
+            ),
+            Container(
+              margin: EdgeInsets.only(right: 80,top: 5,bottom: 5,left: 20),
+              child: TextField(
+                cursorColor: Colors.white,
+                style: TextStyle(
                   color: Colors.white,
                 ),
+                onChanged: (location1){
+                  location=location1;
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                  filled: true,
+                  fillColor: Colors.black54,
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+                controller: controller4,
               ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.only(right: 80,top: 5,bottom: 5,left: 20),
-            child: TextField(
-              cursorColor: Colors.white,
-              style: TextStyle(
-                color: Colors.white,
-              ),
-              onChanged: (location1){
-                location=location1;
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                ),
-                filled: true,
-                fillColor: Colors.black54,
-                hintStyle: TextStyle(
-                  color: Colors.grey,
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                child: Text(
+                    "Give a Description",
+                  style: GoogleFonts.poppins(
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-              controller: controller4,
             ),
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
-              child: Text(
-                  "Give a Description",
-                style: GoogleFonts.poppins(
-                  fontSize: 15,
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                maxLines: 5,
+                style: TextStyle(
                   color: Colors.white,
                 ),
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 15),
-            child: TextField(
-              maxLines: 5,
-              style: TextStyle(
-                color: Colors.white,
-              ),
-              maxLength: 300,
-              onChanged: (DESCRIPTION){
-                description=DESCRIPTION;
-              },
-              decoration: InputDecoration(
+                maxLength: 300,
+                onChanged: (DESCRIPTION){
+                  description=DESCRIPTION;
+                },
+                decoration: InputDecoration(
 
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                  filled: true,
+                  fillColor: Colors.black54,
                 ),
-                filled: true,
-                fillColor: Colors.black54,
+                controller: controller3,
               ),
-              controller: controller3,
+            ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    margin: EdgeInsets.only(left: 20,top:5,bottom: 5),
+                    child: Text(
+                      "Add Images (Optional, yet helpful! )",
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 150,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    children: <Widget>[
+                      Container(
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.add_circle,
+                            size: 35,
+                            color: Colors.white,
+                          ),
+                          onPressed: (){
+
+                          },
+                        ),
+                        margin: EdgeInsets.symmetric(vertical: 3,horizontal: 10),
+                        height: MediaQuery.of(context).size.height*0.6,
+                        width:  MediaQuery.of(context).size.width*0.45,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.black54,
+                        ),
+                      ),
+                      Container(
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.add_circle,
+                            size: 35,
+                            color: Colors.white,
+                          ),
+                          onPressed: (){
+
+                          },
+                        ),
+                        margin: EdgeInsets.symmetric(vertical: 3,horizontal: 10),
+                        height: MediaQuery.of(context).size.height*0.6,
+                        width:  MediaQuery.of(context).size.width*0.45,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.black54
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              Container(
+                margin: EdgeInsets.only(top: 15),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20)
+            ),
+            child: RawMaterialButton(
+              onPressed: (){
+
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical:5.0, horizontal: 20.0),
+                child: Text(
+                  "Submit",
+                  style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ),
           ),
         ],
-      )),
+      ),
+
+    ],
+    ),
+      ),
     );
   }
 }
